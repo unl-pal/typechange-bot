71cf60dd4523c7ecf1182adf292968ca104fa907
Samuel W. Flint <swflint@flintfam.org>
2024-11-22 12:26:54 -0600

    Add default celery broker URL

--- typechangesapp/settings.py
+++ typechangesapp/settings.py
@@ -128,4 +128,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CELERY_TIMEZONE = "America/Chicago"
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
+CELERY_BROKER_URL = "redis://localhost:6379/0"
 CELERY_RESULT_BACKEND = 'django-db'
