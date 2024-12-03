7e5b87ad9a1d2992bcf2f784e40a243c544a8598
Samuel W. Flint <swflint@flintfam.org>
2024-12-03 09:24:07 -0600

    Commits are only processed if the project is tracked

--- survey/tasks.py
+++ survey/tasks.py
@@ -22,13 +22,14 @@ optout_command = re.compile(f'@{settings.GITHUB_APP_NAME}\\soptout', re.IGNORECA
 def process_push_data(owner, repo, commits):
     project = Project.objects.get(Q(owner=owner) & Q(name=repo))
 
-    for commit_data in commits:
-        commit = Commit(project=project,
-                        hash=commit_data['id'],
-                        message=commit_data['message'],
-                        diff = '\n'.join(commit_data['modified']))
-        commit.save()
-        process_commit.delay(commit.pk)
+    if project.track_changes:
+        for commit_data in commits:
+            commit = Commit(project=project,
+                            hash=commit_data['id'],
+                            message=commit_data['message'],
+                            diff = '\n'.join(commit_data['modified']))
+            commit.save()
+            process_commit.delay(commit.pk)
 
 @app.task()
 def process_commit(commit_pk):
