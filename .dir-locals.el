656cc2c8c18d4bd5ce7d3c8f73785e342ea5fa61
Samuel W. Flint <swflint@flintfam.org>
2024-12-03 09:25:44 -0600

    Add python related configuration

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((python-ts-mode . ((python-shell-interpreter . "python3")
+                    (python-shell-interpreter-args . "manage.py shell"))))
