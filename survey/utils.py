a0f64b84d7ab6e7de6dab11760ed42764aa189a0
Samuel W. Flint <swflint@flintfam.org>
2024-12-17 13:16:14 -0600

    Actually make typechecker detection work

--- survey/utils.py
+++ survey/utils.py
@@ -3,24 +3,24 @@
 
 import tomllib
 
-def get_typechecker_configuration(repo, language):
+def get_typechecker_configuration(repo, language, commit_like='HEAD'):
     typecheckers = []
     if language == 'Python':
-        for object in repo.active_branch.tree.traverse():
+        for object in repo.rev_parse(commit_like).tree.traverse():
             if object.type == 'blob':
-                if name in ['mypy.ini',
+                if object.name in ['mypy.ini',
                             '.mypy.ini',
                             '.pyre_configuration',
                             'pytype.toml',
                             'pyrightconfig.json']:
-                    typecheckers += object.path
+                    typecheckers.append(object.path)
 
-                elif name == 'pyproject.toml':
-                    data = tomllib.loads(object.data_stream.read())
+                elif object.name == 'pyproject.toml':
+                    data = tomllib.loads(object.data_stream.read().decode())
                     if 'tool' in data.keys():
                         for tool in ['mypy', 'pytype', 'pyright']:
                             if tool in data['tool'].keys():
-                                typecheckers += f'{object.path}[tool.{tool}]'
+                                typecheckers.append(f'{object.path}[tool.{tool}]')
 
         if len(typecheckers) > 0:
             return '\n'.join(typecheckers)
