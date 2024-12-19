b0181c604048d92784b2c780a0b06c13143d59a2
Samuel W. Flint <swflint@flintfam.org>
2024-12-19 16:45:21 -0600

    Actions can be collected using the json output format...

--- survey/ast_diff.py
+++ survey/ast_diff.py
@@ -7,27 +7,22 @@ from pathlib import Path
 from typing import Union, Optional
 import subprocess
 from subprocess import CalledProcessError
+import json
 
 GUMTREE_PATH = 'gumtree'
 
 class AstDiff:
 
     def __init__(self, pre_diff_version: Union[str, Path], post_diff_version: Union[str, Path]):
-        diff_proc = subprocess.run([GUMTREE_PATH, 'textdiff', str(pre_diff_version), str(post_diff_version)],
+        diff_proc = subprocess.run([GUMTREE_PATH, 'textdiff', '-f', 'json', str(pre_diff_version), str(post_diff_version)],
                                    capture_output=True,
                                    check=True)
-        self.diff_data = diff_proc.stdout
-        if len(self.diff_data) == 0:
+        if len(diff_proc.stdout.decode()) == 0:
             raise ValueError("AST Diff Generation Failed, no output.",
                              diff_proc.stderr.decode())
+        self._diff_data_json = diff_proc.stdout.decode()
+        self._diff_json = json.loads(self._diff_data_json)
 
-        cluster_proc = subprocess.run([GUMTREE_PATH, 'cluster', str(pre_diff_version), str(post_diff_version)],
-                                      capture_output=True,
-                                      check=True)
-        self.cluster_data = cluster_proc.stdout
-        if len(self.cluster_data) == 0:
-            raise ValueError("Diff Cluster Generation Failed, no output.",
-                             cluster_proc.stderr.decode())
 
     @classmethod
     def from_diff(cls, diff: Diff, suffix: Optional[str] = None):
