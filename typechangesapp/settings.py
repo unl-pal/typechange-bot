24fd08dbfbcf3c65dbf408f5f7d6f32e3f5cb48c
Samuel W. Flint <swflint@flintfam.org>
2025-01-29 12:08:07 -0600

    Add some static file configuration

--- typechangesapp/settings.py
+++ typechangesapp/settings.py
@@ -120,7 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+URL_ROOT = config('URL_ROOT', default='/')
+STATIC_URL = URL_ROOT + 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = URL_ROOT + 'media/'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
