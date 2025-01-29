dbdcadec66250d5b2f17677efd3f4684cbdfd018
Samuel W. Flint <swflint@flintfam.org>
2025-01-29 12:08:41 -0600

    Fix an import error

--- typechangesapp/settings.py
+++ typechangesapp/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import dj_database_url
 from decouple import config, Csv
+import os.path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
