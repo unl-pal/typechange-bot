3acac08af6aab20c3d0c6778eedf1986d34fdca1
Samuel W. Flint <swflint@flintfam.org>
2025-02-07 15:12:50 -0600

    Bot name is all lower-case

--- typechangesapp/settings.py
+++ typechangesapp/settings.py
@@ -138,7 +138,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 DATA_DIR = config('DATA_DIR', default='./data/', cast=Path)
 
 # GitHub Settings
-GITHUB_APP_NAME = config('GITHUB_APP_NAME', default='TypeChangeBot')
+GITHUB_APP_NAME = config('GITHUB_APP_NAME', default='typechangebot')
 GITHUB_APP_KEY = config('GITHUB_APP_KEY', default='FOO').replace('\\n', '\n')
 GITHUB_APP_ID = config('GITHUB_APP_ID', default='FOO')
 
