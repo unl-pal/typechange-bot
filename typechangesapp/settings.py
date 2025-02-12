2d7fbe4d43c183a2486f0adc23165cc5dcd3d196
Samuel W. Flint <swflint@flintfam.org>
2025-02-12 14:13:45 -0600

    Enable Django Celery Beat

--- typechangesapp/settings.py
+++ typechangesapp/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_celery_results',
+    'django_celery_beat',
     'django_extensions',
     'treebeard',
     'markdownx'
