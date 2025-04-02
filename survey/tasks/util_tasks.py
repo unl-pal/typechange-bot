7a8f7f72bc7358e177c3375f9a367a41f534a75c
Samuel W. Flint <swflint@flintfam.org>
2025-04-02 10:38:49 -0500

    Add maintainer emailing

--- survey/tasks/util_tasks.py
+++ survey/tasks/util_tasks.py
@@ -2,10 +2,31 @@
 # coding: utf-8
 
 from .common import app, current_node
+from survey.models import ProjectCommitter
 
-from django.core.mail import EmailMessage
+from django.core.mail import EmailMessage, EmailMultiAlternatives
+from django.template import loader
 
 @app.task()
 def send_email(subject, message, **kwargs):
     email = EmailMessage(subject, message, **kwargs)
     email.send()
+
+@app.task()
+def send_maintainer_email(maintainer_id: int):
+    maintainer = ProjectCommitter.objects.get(id=maintainer_id)
+    if maintainer.is_maintainer and not maintainer.has_been_emailed and maintainer.email_address is not None:
+        template_data = {
+            'NAME': f'{maintainer.committer.username}',
+            'PROJECT': f'{maintainer.project.owner}/{maintainer.project.name}'
+        }
+        html_template = loader.get_template('maintainer-request.html')
+        message_html = html_template.render(template_data)
+        text_template = loader.get_template('maintainer-request.txt')
+        message_text = text_template.render(template_data)
+        message = EmailMultiAlternatives(f'Research Opportunity: Can we monitor {maintainer.project.owner}/{maintainer.project.name}?', message_html, to=[maintainer.email_address])
+        message.content_subtype = "html"
+        message.attach_alternative(message_text, 'text/plain')
+        message.send()
+        maintainer.has_been_emailed = True
+        maintainer.save()
