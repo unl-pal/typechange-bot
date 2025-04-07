49b4b4c1ba47061a631eb3256e2759374adf704b
Samuel W. Flint <swflint@flintfam.org>
2025-04-07 13:37:17 -0500

    Move email location

--- survey/models.py
+++ survey/models.py
@@ -96,6 +96,7 @@ class MaintainerReason(NS_Node):
 
 class Committer(models.Model):
     username = models.CharField(max_length=200)
+    email_address = models.EmailField(null=True)
     initial_contact_date = models.DateTimeField("initial contact date", auto_now_add=True, editable=False)
     last_contact_date = models.DateTimeField("date of last contact", auto_now=True, editable=False)
     consent_timestamp = models.DateTimeField("date of consent", null=True, editable=False)
@@ -170,7 +171,6 @@ class ProjectCommitter(models.Model):
     committer = models.ForeignKey(Committer, on_delete=models.CASCADE, editable=False)
     is_maintainer = models.BooleanField(null=True)
     has_been_emailed = models.BooleanField(default=False, null=False)
-    email_address = models.EmailField(null=True)
     maintainer_survey_response = models.TextField('response to maintainer survey', null=True, blank=True, editable=False)
     initial_commit = models.ForeignKey('Commit', on_delete=models.CASCADE, editable=False, null=True)
     tags = models.ManyToManyField (MaintainerReason)
