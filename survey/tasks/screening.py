1940cc5ae9486a2f2698c9c713e553f4af3bca21
Samuel W. Flint <swflint@flintfam.org>
2025-04-16 13:18:26 -0500

    Use the checker for language files

--- survey/tasks/screening.py
+++ survey/tasks/screening.py
@@ -6,7 +6,7 @@ from .common import current_node, app
 from survey.models import Project
 from django.utils import timezone
 
-from survey.utils import get_typechecker_configuration, has_annotations
+from survey.utils import get_typechecker_configuration, has_annotations, has_language_file
 from git import Repo
 from tempfile import TemporaryDirectory
 from pathlib import Path
@@ -15,19 +15,28 @@ from pathlib import Path
 def prescreen_project(project_id: int) -> None:
     project: Project = Project.objects.get(id=project_id)
 
+    project.has_language_files = False
+    project.has_typechecker_configuration = False
+    project.typechecker_files = None
+    project.annotations_detected = False
+
     with TemporaryDirectory() as temp_dir:
         git_repo = Repo.clone_from(project.clone_url, temp_dir,
                                    multi_options=[ "--depth 1",
                                                    "--shallow-submodules",
                                                    "--no-remote-submodules" ])
-        typechecker_config = get_typechecker_configuration(git_repo, project.language)
-        if typechecker_config is not None:
-            project.typechecker_files = typechecker_config
-            project.has_typechecker_configuration = True
 
-        if has_annotations(git_repo, project.language):
-            project.annotations_detected = True
+        project.has_language_files = has_language_file(git_repo, project.language)
 
-    project.track_changes = project.has_typechecker_configuration or project.annotations_detected
+        if project.has_language_files:
+            typechecker_config = get_typechecker_configuration(git_repo, project.language)
+            if typechecker_config is not None:
+                project.typechecker_files = typechecker_config
+                project.has_typechecker_configuration = True
+
+            project.annotations_detected = has_annotations(git_repo, project.language)
+
+
+    project.track_changes = project.has_language_files and (project.has_typechecker_configuration or project.annotations_detected)
 
     project.save()
