6baadf406c5b2a26bdc8d7b63f06654da2a21c11
Samuel W. Flint <swflint@flintfam.org>
2025-05-01 08:03:00 -0500

    Only send emails if maintained projects are trackable

--- survey/tasks/util_tasks.py
+++ survey/tasks/util_tasks.py
@@ -16,18 +16,20 @@ def send_email(subject, message, **kwargs):
 @app.task()
 def send_maintainer_email(committer_id: int):
     committer = Committer.objects.get(id=committer_id)
+    maintained_tracked_projects = committer.projectcommitter_set.filter(is_maintainer=True, project__track_changes=True).count()
     if committer.email_address is not None and not committer.has_been_emailed:
-        template_data = { 'committer': committer }
-        html_template = loader.get_template('maintainer-request.html')
-        message_html = html_template.render(template_data)
-        text_template = loader.get_template('maintainer-request.txt')
-        message_text = text_template.render(template_data)
-        message = EmailMultiAlternatives(f'Research Opportunity: Can we monitor your projects?',
-                                         message_html,
-                                         to=[committer.formatted_email_address],
-                                         reply_to=[f'{settings.ADMIN_NAME} <{settings.ADMIN_EMAIL}>'])
-        message.content_subtype = "html"
-        message.attach_alternative(message_text, 'text/plain')
-        message.send()
+        if maintained_tracked_projects > 0:
+            template_data = { 'committer': committer }
+            html_template = loader.get_template('maintainer-request.html')
+            message_html = html_template.render(template_data)
+            text_template = loader.get_template('maintainer-request.txt')
+            message_text = text_template.render(template_data)
+            message = EmailMultiAlternatives(f'Research Opportunity: Can we monitor your projects?',
+                                             message_html,
+                                             to=[committer.formatted_email_address],
+                                             reply_to=[f'{settings.ADMIN_NAME} <{settings.ADMIN_EMAIL}>'])
+            message.content_subtype = "html"
+            message.attach_alternative(message_text, 'text/plain')
+            message.send()
         committer.has_been_emailed = True
         committer.save()
