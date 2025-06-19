a7438b30b2986a5977535420710ab5b70c962049
Samuel W. Flint <swflint@flintfam.org>
2025-06-19 08:53:39 -0500

    Add dir locals notes

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,6 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((python-ts-mode . ((python-shell-interpreter . "python3")
+((nil . ((denote-project-notes-identifier . (("swflint" . "20241126T084245")))))
+ (python-ts-mode . ((python-shell-interpreter . "python3")
                     (python-shell-interpreter-args . "manage.py shell"))))
