36d7b732f5b75586baae7763980c4b081a6e9bb9
Samuel W. Flint <swflint@flintfam.org>
2025-07-15 12:11:51 -0500

    Drop some python configuration

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,4 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((denote-project-notes-identifier . (("swflint" . "20241126T084245")))))
- (python-ts-mode . ((python-shell-interpreter . "python3")
-                    (python-shell-interpreter-args . "manage.py shell"))))
+((nil . ((denote-project-notes-identifier . (("swflint" . "20241126T084245"))))))
