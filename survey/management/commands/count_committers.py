38cdb2dfaf6baab34f72f815e3191f4d0394a544
Samuel W. Flint <swflint@flintfam.org>
2025-07-16 12:56:06 -0500

    Add a script to count committers

---
+++ survey/management/commands/count_committers.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from django.core.management.base import BaseCommand
+from survey.models import Project
+
+class Command(BaseCommand):
+    help = "Count committers in projects."
+
+    def handle(self, *args, **options):
+        node = Node.objects.get(hostname=gethostname())
+        for project in Project.objects.filter(track_changes=True, installation_id__isnull=False, num_committers__isnull=True):
+            try:
+                print(f'Counting commits in project {project}...')
+                count = len({ s.author.login for s in project.gh.get_stats_contributors() if s.author is not None })
+                project.num_committers = count
+                project.save()
+            except KeyboardInterrupt as ex:
+                raise ex
+            except:
+                print(f'failed to count committers for {project}')
