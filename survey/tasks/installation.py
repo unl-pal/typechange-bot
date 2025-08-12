c19753e590d553e1c865bd6a80cd45c19c7656c4
Samuel W. Flint <swflint@flintfam.org>
2025-08-12 08:41:18 -0500

    Don't delete repositories

--- survey/tasks/installation.py
+++ survey/tasks/installation.py
@@ -86,18 +86,18 @@ def process_installation_repositories(payload):
                 owner, name = repo['full_name'].split('/')
                 if Project.objects.filter(owner=owner, name=name).count() > 0:
                     for project in Project.objects.filter(owner=owner, name=name):
-                        deleted_repo = DeletedRepository(node = project.host_node,
-                                                         owner = project.owner,
-                                                         name = project.name,
-                                                         reason = DeletedRepository.DeletionReason.DELETED)
-                        if project.data_subdir is not None:
-                            deleted_repo.subdir = project.data_subdir
-                        deleted_repo.save()
+                        # deleted_repo = DeletedRepository(node = project.host_node,
+                        #                                  owner = project.owner,
+                        #                                  name = project.name,
+                        #                                  reason = DeletedRepository.DeletionReason.DELETED)
+                        # if project.data_subdir is not None:
+                        #     deleted_repo.subdir = project.data_subdir
+                        # deleted_repo.save()
 
                         project.installation_id = None
-                        project.data_subdir = None
+                        # project.data_subdir = None
                         project.remove_date = timezone.now()
-                        project.track_changes = False
+                        project.track_changes = True
                         project.save()
 
 
